796576df0627af27e881e65bbe097a404d742bb6
sqrtqiezi <njin.cool@gmail.com>
2017-11-16 14:50:38 +0800

    feature: 完成游戏功能

--- js/app.js
+++ js/app.js
@@ -1,38 +1,132 @@
-/*
- * Create a list that holds all of your cards
- */
+var Game = function () {
+    this.cards = ['diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb',
+        'diamond', 'paper-plane-o', 'anchor', 'bolt', 'cube', 'leaf', 'bicycle', 'bomb'
+    ];
+    this.moves = 3;
+    this.matchedCount = 0;
+    this.steps = 0;
+    this.threshold = 20;
+    this.checkCards = [];
+    this.waiting = false;
+};
 
+Game.prototype = {
+    start: function start() {
+        this.shuffle();
+        this.renderAll();
+        this.bindGameEvent();
+    },
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+    restart: function restart() {
+        this.moves = 3;
+        this.matchedCount = 0;
+        this.steps = 0;
+        this.threshold = 20;
+        this.checkCards = [];
+        this.shuffle();
+        this.renderAll();
+    },
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    renderAll: function renderAll() {
+        this.renderStars();
+        this.renderCards();
+    },
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    renderStars: function renderStars() {
+        var html = [];
+        for (var i = 0; i < this.moves; i++) {
+            html.push('<li><i class="fa fa-star"></i></li>');
+        }
+        document.getElementsByClassName('stars')[0].innerHTML = html.join('');
+        document.getElementsByClassName('moves')[0].innerHTML = this.moves;
+    },
+
+    renderCards: function renderCards() {
+        html = [];
+        this.cards.forEach(function (item) {
+            html.push(`<li class="card show" data-card="${item}">
+                <i class="fa fa-${item}"></i>
+            </li>`);
+        });
+        document.getElementsByClassName('deck')[0].innerHTML = html.join('');
+        this.$cards = document.getElementsByClassName('card');
+        this.bindCardsEvent();
+    },
+
+    bindCardsEvent: function bindCardsEvent() {
+        var self = this;
+
+        [].forEach.call(this.$cards, function (item, index) {
+            //TODO: 添加翻牌动效
+            item.addEventListener('click', function () {
+                if (self.waiting ||
+                    self.checkCards.length === 2 ||
+                    this.classList.contains('open')
+                ) {
+                    return;
+                }
+
+                this.classList.add('open');
+                this.classList.add('show');
+                self.checkCards.push(self.$cards[index]);
+                if (self.checkCards.length === 2) {
+                    self.check();
+                }
+
+                self.steps++;
+                if (self.steps >= self.threshold) {
+                    self.moves--;
+                    self.steps = 0;
+                    self.renderStars();
+                    if(self.moves <= 0 && self.count !== self.cards.length) {
+                        alert('Game Over!');
+                    }
+                }
+            });
+        });
+    },
+
+    check: function check() {
+        var self = this;
+        self.waiting = true;
+
+        setTimeout(function () {
+            if (self.checkCards[0].dataset.card === self.checkCards[1].dataset.card) {
+                self.checkCards.forEach(function (item) {
+                    item.classList.add('match');
+                });
+                self.matchedCount += 2;
+                if (self.matchedCount === self.cards.length) {
+                    alert('Winner! Winner! Chiken Dinner!');
+                }
+            } else {
+                //TODO: 添加猜测错误时的样式 
+                self.checkCards.forEach(function (item) {
+                    item.classList.remove('open');
+                    item.classList.remove('show');
+                });
+            }
+            self.checkCards = [];
+            self.waiting = false;
+        }, 800);
+    },
+
+    bindGameEvent: function bindGameEvent() {
+        document.getElementsByClassName('restart')[0].addEventListener('click', this.restart.bind(this));
+    },
+
+    shuffle: function shuffle() {
+        var currentIndex = this.cards.length,
+            temporaryValue, randomIndex;
+
+        while (currentIndex !== 0) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = this.cards[currentIndex];
+            this.cards[currentIndex] = this.cards[randomIndex];
+            this.cards[randomIndex] = temporaryValue;
+        }
     }
+};
 
-    return array;
-}
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+new Game().start();
